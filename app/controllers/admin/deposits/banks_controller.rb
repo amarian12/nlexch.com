32817f3435459e67bb6e2227c20cff783a2f785d
Irwan <irwan_gunardi@hotmail.com>
2017-09-26 10:57:04 +0800

    Change one day admin deposit to two days

--- app/controllers/admin/deposits/banks_controller.rb
+++ app/controllers/admin/deposits/banks_controller.rb
@@ -4,7 +4,7 @@ module Admin
       load_and_authorize_resource :class => '::Deposits::Bank'
 
       def index
-        start_at = DateTime.now.ago(60 * 60 * 24)
+        start_at = DateTime.now.ago(60 * 60 * 24 * 2)
         @oneday_banks = @banks.includes(:member).
           where('created_at > ?', start_at).
           order('id DESC')
